a29a74f0e46b657a9f51e4cc0d3602c2547c168f
FatemaAlsheno <202102003@stu.uob.edu.bh>
2024-11-29 14:57:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 
-$url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100"
+//retreive data from API and display response
 
+$url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100"
 $response = file_get_contents($url);
 echo $response;
 print_r($response);
+
+// process API response and handle errors 
 
 $data = json_decode($response, true);
 echo $response;
@@ -13,18 +16,24 @@ print_r($response);
 if(!$data || !isset($data["results"])){
     die('error fetching data from API')
 }
-
 $result = $data["results"];
 
 ?>
+
 <html>
     <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+// layout and styling
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
 </head>
     <body>
 <table>
     <thead>
+        
+// html table to display the retrieved data
+        
 <tr>
     <th>Year</th>
     <th>Semester</th>
@@ -34,6 +43,9 @@ $result = $data["results"];
     <th>Number of students</th>
 </tr>
     </thead>
+    
+// generate table from a php array named $result
+    
     <tbody>
         <?php
         foreach($result as $student){
